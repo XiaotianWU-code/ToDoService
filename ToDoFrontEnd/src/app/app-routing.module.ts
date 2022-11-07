6088ebdcd5b913b703256c0a1d41251f7a9b8e37
Xiaotian WU <XWu32@slb.com>
2022-11-07 14:19:43 +0800

    feat: add router

--- ToDoFrontEnd/src/app/app-routing.module.ts
+++ ToDoFrontEnd/src/app/app-routing.module.ts
@@ -1,10 +1,33 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { CreateTodoitemComponent } from './todo/create-todoitem/create-todoitem.component';
+import { ListTodoitemComponent } from './todo/list-todoitem/list-todoitem.component';
+import { TodoitemDetailComponent } from './todo/todoitem-detail/todoitem-detail.component';
+import { UpdateTodoItemComponent } from './todo/update-todo-item/update-todo-item.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/todos', pathMatch: 'full' },
+  {
+    path: 'todos',
+    component: ListTodoitemComponent,
+  },
+  {
+    path: 'todos/create',
+    component: CreateTodoitemComponent,
+  },
+  {
+    path: 'todos/:id',
+    component: TodoitemDetailComponent,
+  },
+  {
+    path: 'todos/edit/:id',
+    component: UpdateTodoItemComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
